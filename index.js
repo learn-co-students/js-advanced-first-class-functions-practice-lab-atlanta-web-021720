7003a67383ae234f05af2738cf6a472ed532e58a
gcampbell391 <gcampbell391@gmail.com>
2020-03-30 15:55:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+
+
+
+function logDriverNames(drivers) {
+    for (const driver of drivers) {
+        console.log(driver.name)
+    }
+}
+
+function logDriversByHometown(drivers, location) {
+    for (const driver of drivers) {
+        if (driver.hometown === location) {
+            console.log(driver.name)
+        }
+    }
+}
+
+function driversByRevenue(drivers) {
+    return drivers.slice().sort((a, b) => a.revenue - b.revenue);
+}
+
+function driversByName(drivers) {
+    return drivers.slice().sort((a, b) => a.name.localeCompare(b.name));
+}
+
+function totalRevenue(drivers) {
+    let totalPrice = 0;
+
+    drivers.forEach(function(driver) {
+        totalPrice += driver.revenue;
+    });
+
+    return totalPrice;
+}
+
+function averageRevenue(drivers) {
+    return totalRevenue(drivers) / drivers.length
+}
